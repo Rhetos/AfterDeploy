222410a0b19493536241c7f4ea26f5672365c16a
Bojan Antolović <bojan.antolovic@gmail.com>
2018-04-23 09:55:32 +0200

    Improved build: VS2017 command-line support. Removed relative path reference to the Rhetos source.

--- Build.bat
+++ Build.bat
@@ -1,21 +1,26 @@
-@REM HINT: USE /NOPAUSE PARAMETER WHEN AUTOMATING THE BUILD.
+SETLOCAL
+SET Version=1.2.0
+SET Prerelease=auto
 
-IF NOT DEFINED VisualStudioVersion CALL "%VS140COMNTOOLS%VsDevCmd.bat" || ECHO ERROR: Cannot find Visual Studio 2015, missing VS140COMNTOOLS variable. && GOTO Error0
+REM Find Visual Studio 2015 or newer. VS2017 uses vswhere.exe for locating, VS2015 uses %VS140COMNTOOLS%.
+@IF DEFINED VisualStudioVersion GOTO EndVcvarsall
+FOR /f "usebackq delims=" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -prerelease -latest -property installationPath`) do (
+  IF EXIST "%%i\Common7\Tools\VsDevCmd.bat" (CALL "%%i\Common7\Tools\VsDevCmd.bat" && GOTO EndVcvarsall || GOTO Error0)
+)
+IF "%VS140COMNTOOLS%" NEQ "" (CALL "%VS140COMNTOOLS%VsDevCmd.bat" x86 && GOTO EndVcvarsall || GOTO Error0)
+@ECHO ERROR: Cannot find Visual Studio.
+@GOTO Error0
+:EndVcvarsall
 @ECHO ON
 
-PUSHD "%~dp0" || GOTO Error0
-CALL ChangeVersions.bat || GOTO Error1
-IF EXIST msbuild.log DEL msbuild.log || GOTO Error1
-
-WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error1
-
-NuGet.exe restore Rhetos.AfterDeploy.sln -NonInteractive || GOTO Error1
-MSBuild.exe Rhetos.AfterDeploy.sln /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error1
+PowerShell .\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
+WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
+NuGet restore -NonInteractive || GOTO Error0
+MSBuild /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error0
 IF NOT EXIST Install md Install
-NuGet.exe pack -o Install || GOTO Error1
-
-CALL ChangeVersions.bat /RESTORE || GOTO Error1
-POPD
+NuGet pack -OutputDirectory Install || GOTO Error0
+REM Updating the version of all projects back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
+PowerShell .\ChangeVersion.ps1 %Version% dev || GOTO Error0
 
 @REM ================================================
 
@@ -23,10 +28,8 @@ POPD
 @ECHO %~nx0 SUCCESSFULLY COMPLETED.
 @EXIT /B 0
 
-:Error1
-@POPD
 :Error0
 @ECHO.
 @ECHO %~nx0 FAILED.
-@IF /I [%2] NEQ [/NOPAUSE] @PAUSE
+@IF /I [%1] NEQ [/NOPAUSE] @PAUSE
 @EXIT /B 1
--- Readme.md
+++ Readme.md
@@ -1,7 +1,10 @@
-﻿Rhetos.AfterDeploy is a DSL package for [Rhetos development platform](https://github.com/Rhetos/Rhetos).
+﻿# AfterDeploy
+
+Rhetos.AfterDeploy is a plugin package for [Rhetos development platform](https://github.com/Rhetos/Rhetos).
 It simplifies the handling of SQL scripts that need to be executed on each deployment.
 
-Features:
+## Features
+
 * This plugin will activate inside `DeployPackages.exe`, after database is upgraded.
 * It will execute all SQL scripts placed in the `AfterDeploy` subfolder of each deployed package.
 * The ordering of SQL scripts will respect dependencies between packages, defined in their .nuspec files.
